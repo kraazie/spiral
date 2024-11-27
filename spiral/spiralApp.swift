321eaeea63d30cfbc0138a2f501d9a5f76c504e8
Ali Ghani <ali48pk@hotmail.com>
2024-11-27 18:11:09 +0500

    Initial Commit

---
+++ spiral/spiralApp.swift
@@ -0,0 +1,17 @@
+//
+//  spiralApp.swift
+//  spiral
+//
+//  Created by apple on 27/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct spiralApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ spiralTests/spiralTests.swift
@@ -0,0 +1,17 @@
+//
+//  spiralTests.swift
+//  spiralTests
+//
+//  Created by apple on 27/11/2024.
+//
+
+import Testing
+@testable import spiral
+
+struct spiralTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
